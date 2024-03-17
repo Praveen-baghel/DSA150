39639dfeb2581c604881df8c646d9d64b089b4e6
Praveen-baghel <praveenbaghel88990@gmail.com>
2024-03-17 22:45:45 +0530

    Solution45

--- Solution45.java
+++ Solution45.java
@@ -1,5 +1,13 @@
 public class Solution45 {
     public int jump(int[] nums) {
-        return 0;
+        int jump = 0, destination = 0, currentMaxJump = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            currentMaxJump = Math.max(currentMaxJump, i + nums[i]);
+            if (i == destination) {
+                jump++;
+                destination = currentMaxJump;
+            }
+        }
+        return jump;
     }
 }
--- Solution55.java
+++ Solution55.java
@@ -1,6 +1,6 @@
 public class Solution55 {
     public boolean canJump(int[] nums) {
-        int res = nums[nums.length - 1];
+        int res = nums.length - 1;
         for (int i = nums.length - 1; i >= 0; i--) {
             if (i + nums[i] >= res)
                 res = i;
