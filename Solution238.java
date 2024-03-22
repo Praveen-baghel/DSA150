e17e5de688d2aabefeec3f1bbe7e2c059e0ddf54
Praveen-baghel <praveenbaghel88990@gmail.com>
2024-03-22 23:55:11 +0530

    Solution238

---
+++ Solution238.java
@@ -0,0 +1,23 @@
+public class Solution238 {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = 1;
+        right[n - 1] = 1;
+        int product = 1;
+        for (int i = 1; i < n; i++) {
+            product = product * nums[i - 1];
+            left[i] = product;
+        }
+        product = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            product = product * nums[i + 1];
+            right[i] = product;
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = left[i] * right[i];
+        }
+        return nums;
+    }
+}
